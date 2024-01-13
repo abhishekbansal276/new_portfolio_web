b45a6b020387c9000b1ed1115f5e2539b0b742de
Abhishek Bansal <73697686+abhishekbansal276@users.noreply.github.com>
2024-01-13 15:47:05 +0530

    Initial Commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+// Typing animation
+var typed = new Typed(".typing", {
+    strings:["","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
+
+var typed = new Typed(".typing_one", {
+    strings:["","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
+
